e6af275f69152ac9a78b476df3c58132a0a6893b
sathish-RG <sathish958527@gmail.com>
2025-06-17 09:15:05 +0530

    footer-update

--- src/components/student/Footer.jsx
+++ src/components/student/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
 
       </div>
       <p className="py-4 text-center text-xs md:text-sm text-white/60">
-        Copyright 2024 © GreatStack. All Right Reserved.
+        Copyright 2025 Edemy. All Right Reserved.
       </p>
     </footer>
   );
